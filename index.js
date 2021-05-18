5462c6ff01826b7858c02b252e0b634e2b5defec
André <andreglock@gmail.com>
2021-05-18 13:24:14 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 class Employee {
 	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.fullname = `${firstname} ${lastname}`;
+		this.email = `${firstname.toLowerCase()}.${lastname.toLowerCase()}@company.com`;
 	}
 }
+
+emp1 = new Employee("John", "Smith");
+console.log(emp1.fullname);
+
+emp2 = new Employee("Mary",  "Sue");
+console.log(emp2.email);
+
+emp3 = new Employee("Antony", "Walker");
+console.log(emp3.firstname);
